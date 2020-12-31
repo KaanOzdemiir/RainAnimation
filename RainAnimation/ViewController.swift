fd7de2221a91e79e3d0baf2a1d481ed464b5d311
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2020-12-31 16:50:56 +0300

    Initial Commit

---
+++ RainAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RainAnimation
+//
+//  Created by Kaan Ozdemir on 31.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
